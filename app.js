08de5526af5ca4eaab65540355f55e3b9c647140
ivanmedsdirect <143231629+ivanmedsdirect@users.noreply.github.com>
2023-08-26 00:40:26 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,86 @@
+const header = document.querySelector(".header");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function(){
+    const scrollHeight = window.pageYOffset;
+    const headerHeight = header.getBoundingClientRect().height;
+    if (scrollHeight > headerHeight) {
+        header.classList.add("fixed-nav");
+        console.log(headerHeight)
+    } else{
+        header.classList.remove("fixed-nav");
+    }
+    if (scrollHeight > 500) {
+        topLink.classList.add("scroll-link");
+    } else {
+        topLink.classList.remove("scroll-link");
+    }
+})
+
+const navs = document.querySelectorAll (".nav")
+
+    navs.forEach( function (nav){
+        const subNav = nav.querySelector(".sub-nav")
+
+        
+
+        nav.addEventListener("click", function(){
+          /*  console.log("hello");*/
+
+            navs.forEach(function(navo){
+                const subNavo = navo.querySelector(".sub-nav");
+
+                if(navo !== nav) {
+             /*       console.log("not"); */
+                    subNavo.classList.remove("show-sub-nav");
+                }
+            })
+
+            subNav.classList.toggle("show-sub-nav");
+        })
+
+} )
+
+
+window.addEventListener("DOMContentLoaded", function(){
+    const equipmentPicturesContainers = querySelectorAll(".eqipment-pictures-container")   
+    equipmentPicturesContainers.forEach( function(equipmentPicturesContainer){
+        const equipmentParag = equipmentPicturesContainer.querySelector(".equipment-pictures-p");
+        const equipmentPics = equipmentPicturesContainer.querySelector(".equipment-img");
+        const equipmentPicsAlt = equipmentPics.getAttribute("alt"); 
+        })
+   
+    equipmentParag.innerHTML=    equipmentPicsAlt;
+})
+
+const servicesSublists = document.querySelectorAll(".services-sublist");
+
+servicesSublists.forEach(function(servicesSublist){
+   const fontAwesome = servicesSublist.querySelector(".font-awesome");
+   const brownP = servicesSublist.querySelector(".brown-p");
+
+   fontAwesome.addEventListener("click", function(){
+
+   servicesSublists.forEach(function(servicesSublisto){
+    const brownPo = servicesSublisto.querySelector(".brown-p");
+    const fontAwesomeo = servicesSublisto.querySelector(".font-awesome");
+
+        if(fontAwesomeo !== fontAwesome){
+            brownPo.classList.remove("show-brown-p")
+            fontAwesomeo.classList.remove("font-awesome-rotate")
+            
+        }
+   })
+
+    brownP.classList.toggle("show-brown-p")
+    fontAwesome.classList.toggle("font-awesome-rotate")
+   })
+  
+})
+
+const linksContainer = document.querySelector(".links-container");
+const navToggle = document.querySelector(".nav-toggle");
+
+navToggle.addEventListener("click", function() {
+    linksContainer.classList.toggle("show-nav");
+})
